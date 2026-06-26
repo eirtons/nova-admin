c11767f48af4980866629fb6d895e8e8fddd84e3
lagon <xhtons@outlook.com>
2026-06-26 17:27:58 +0800

    新建广告位后跳转列表页

--- src/Filament/Resources/AdSpotResource/Pages/CreateAdSpot.php
+++ src/Filament/Resources/AdSpotResource/Pages/CreateAdSpot.php
@@ -8,4 +8,9 @@ use Inova\NovaAdmin\Filament\Resources\AdSpotResource;
 class CreateAdSpot extends CreateRecord
 {
     protected static string $resource = AdSpotResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
